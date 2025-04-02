5ea1f3dbd3a174863f1b84843a28620a900689e8
Naresh7093 <tallapakan3@gmail.com>
2025-04-02 11:13:35 +0530

    middleware

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ import { db } from "./db/db.js";
 const app = express();
 import userRoute from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
+import { AuthApi } from "./middleware/AuthApi.js";
 app.use(express.json());
 app.use(cookieParser());
 const PORT = process.env.PORT || 3000;
-
-app.get("/", () => {});
 app.use("/api/user", userRoute);
+
+app.get("/home", AuthApi, (req, res) => {
+  console.log(req.userid);
+  console.log("route is running...");
+
+  return res.send("This is home page!");
+});
 db()
   .then(() => {
     app.listen(PORT, (req, res) => {
